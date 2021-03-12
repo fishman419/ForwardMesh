73f1b4f61d243632937754c4ac5860369a575221
mike.yang <mike.yang@ucloud.cn>
2021-03-12 11:50:11 +0800

    forward protocol design

---
+++ protocol.h
@@ -0,0 +1,54 @@
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+#include <stdint.h>
+
+// packet format:
+// |---ForwardHead---|---ForwardNode*n---|---data---|
+// After forward one node:
+// |---ForwardHead---|---ForwardNode*(n-1)---|---data---|
+// reach Final node:
+// |---ForwardHead---|---data---|
+
+const uint32_t kForwardMagic = 0xFF44AADD;
+const uint8_t kForwardVersion1 = 1;
+const uint8_t kMaxForwardTTL = 32;
+
+enum ForwardRetcode {
+  ForwardSuccess = 0,
+  ForwardUnreachable = 1
+};
+
+enum ForwardCommand {
+  ForwardPull = 0,
+  ForwardPush = 1
+};
+
+struct ForwardNode {
+  uint32_t ip;
+  uint32_t port;
+} __attribute__((packed));
+
+struct ForwardRequest {
+  uint32_t length;
+  uint32_t magic;
+  uint8_t version;
+  uint8_t cmd;
+  uint8_t ttl;
+  uint8_t reserved;
+  uint64_t id;
+  uint8_t data[0];
+} __attribute__((packed));
+
+struct ForwardResponse {
+  uint32_t length;
+  uint32_t magic;
+  uint8_t version;
+  uint8_t cmd;
+  uint8_t ttl;
+  uint8_t retcode;
+  uint64_t id;
+  uint8_t data[0];
+} __attribute__((packed));
+
+#endif  // PROTOCOL_H_
