83e27dfd18cff987863783cdf21de581a4b1fc2a
mike.yang <mike.yang@ucloud.cn>
2021-03-25 14:53:27 +0800

    add forward feature

--- fwdd.cc
+++ fwdd.cc
@@ -13,8 +13,92 @@
 
 #include "protocol.h"
 
-int forward_loop(int port) {
+int recv_sync(int fd, void *buffer, int len) {
+  int ret;
+  int offset = 0;
+  while (len > 0) {
+    ret = read(fd, (char *)buffer + offset, len);
+    if (ret < 0) {
+      printf("[FWDD]read error, %d\n", errno);
+      return -1;
+    }
+    offset += ret;
+    len -= ret;
+  }
+  return 0;
+}
+
+int forward_sync(int fd_src, int fd_dst, int len) {
   char buffer[16384];
+  int ret;
+  int write_len;
+  while (len > 0) {
+    ret = read(fd_src, buffer, 16384);
+    if (ret < 0) {
+      printf("[FWDD]read error, %d\n", errno);
+      return -1;
+    }
+    write_len = write(fd_dst, buffer, ret);
+    if (write_len != ret) {
+      printf("[FWDD]write error, %d %d\n", write_len, errno);
+      return -1;
+    }
+    len -= ret;
+  }
+  return 0;
+}
+
+int forward_next(int fd, ForwardRequest *req, ForwardNode *nodes,
+                 ForwardFile *fmeta) {
+  int ret;
+  int next_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (next_fd < 0) {
+    printf("socket error, %d\n", errno);
+    return -1;
+  }
+  struct sockaddr_in dst_addr;
+  dst_addr.sin_family = AF_INET;
+  dst_addr.sin_port = htons(nodes[0].port);
+  dst_addr.sin_addr.s_addr = nodes[0].ip;
+
+  if (connect(next_fd, (struct sockaddr *)&dst_addr, sizeof(dst_addr)) < 0) {
+    printf("connect error, %d\n", errno);
+    return -1;
+  }
+  req->length -= sizeof(ForwardNode);
+  req->ttl -= 1;
+  ret = write(next_fd, req, sizeof(ForwardRequest));
+  if (ret != sizeof(ForwardRequest)) {
+    printf("write error %d\n", errno);
+    return -1;
+  }
+  if (req->ttl) {
+    ret = write(next_fd, nodes + 1, sizeof(ForwardNode) * req->ttl);
+    if (ret != sizeof(ForwardNode) * req->ttl) {
+      printf("write error %d\n", errno);
+      return -1;
+    }
+  }
+  ret = write(next_fd, fmeta, sizeof(ForwardFile) + fmeta->length);
+  if (ret != sizeof(ForwardFile) + fmeta->length) {
+    printf("write error %d\n", errno);
+    return -1;
+  }
+
+  ret = forward_sync(fd, next_fd, req->length - sizeof(ForwardRequest) -
+                                      sizeof(ForwardNode) * req.ttl -
+                                      sizeof(ForwardFile) - fmeta->length);
+  if (ret) {
+    printf("[FWDD]forward data error\n");
+    return -1;
+  }
+
+  close(next_fd);
+  return 0;
+}
+
+int forward_loop(int port) {
+  int ret;
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     printf("[FWDD]socket error, %d\n", errno);
@@ -42,6 +126,8 @@ int forward_loop(int port) {
     struct sockaddr_in src_addr;
     socklen_t addr_len;
     char ip_str[64];
+    ForwardRequest req;
+    ForwardNode *fnodes = NULL;
     int fd = accept(sockfd, (struct sockaddr *)&src_addr, &addr_len);
     if (fd < 0) {
       printf("[FWDD]accept error, %d\n", errno);
@@ -50,95 +136,70 @@ int forward_loop(int port) {
     inet_ntop(AF_INET, &src_addr.sin_addr, ip_str, sizeof(ip_str));
     printf("[FWDD]accept %d, ip: %s, port: %d\n", fd, ip_str,
            ntohs(src_addr.sin_port));
-    uint32_t offset = 0;
-    int len = read(fd, buffer, sizeof(uint32_t));
-    if (len != sizeof(uint32_t)) {
-      printf("[FWDD]read error, %d %d\n", len, errno);
+    ret = recv_sync(fd, &req, sizeof(uint32_t));
+    if (ret) {
+      printf("[FWDD]recv req size\n");
       return -1;
     }
-    offset += sizeof(uint32_t) / sizeof(char);
-    uint32_t data_len = *(uint32_t *)buffer;
-    uint32_t left_len = sizeof(ForwardRequest) - sizeof(uint32_t);
-    ForwardRequest req;
     // ForwardRequest
-    while (left_len != 0) {
-      len = read(fd, buffer + offset, left_len);
-      if (len < 0) {
-        printf("[FWDD]read error, %d\n", errno);
-        return -1;
-      }
-      offset += len;
-      left_len -= len;
+    ret = recv_sync(fd, (char *)&req + sizeof(uint32_t),
+                    sizeof(ForwardRequest) - sizeof(uint32_t));
+    if (ret) {
+      printf("[FWDD]recv req error\n");
+      return -1;
     }
-    memcpy(&req, buffer, sizeof(ForwardRequest));
     printf(
         "[FWDD][header]length %d, magic %d, version %d, cmd %d, ttl %d, id "
         "%d\n",
         req.length, req.magic, req.version, req.cmd, req.ttl, req.id);
     // ForwardNode
     if (req.ttl) {
-      left_len = sizeof(ForwardNode) * req.ttl;
-      offset = 0;
-      ForwardNode *fnodes = (ForwardNode *)malloc(left_len);
-      while (left_len != 0) {
-        len = read(fd, (uint8_t *)fnodes + offset, left_len);
-        if (len < 0) {
-          printf("[FWDD]read error, %d\n", errno);
-          return -1;
-        }
-        offset += len;
-        left_len -= len;
+      fnodes = (ForwardNode *)malloc(sizeof(ForwardNode) * req.ttl);
+      ret = recv_sync(fd, fnodes, sizeof(ForwardNode) * req.ttl);
+      if (ret) {
+        printf("[FWDD]recv nodes error\n");
+        return -1;
       }
       for (uint32_t i = 0; i < req.ttl; ++i) {
         printf("[FWDD][node%u]ip %u, port %u\n", i, fnodes[i].ip,
                fnodes[i].port);
       }
-      free(fnodes);
     }
     // ForwardFile
-    len = read(fd, buffer, sizeof(uint32_t));
-    if (len != sizeof(uint32_t)) {
-      printf("[FWDD]read error, %d %d\n", len, errno);
+    uint32_t f_length;
+    ret = recv_sync(fd, &f_length, sizeof(uint32_t));
+    if (ret) {
+      printf("[FWDD]recv file size error\n");
       return -1;
     }
-    uint32_t f_length = *(uint32_t *)buffer;
     ForwardFile *fmeta = (ForwardFile *)malloc(sizeof(ForwardFile) + f_length);
     fmeta->length = f_length;
-    left_len = f_length;
-    offset = 0;
-    while (left_len != 0) {
-      len = read(fd, fmeta->filename + offset, left_len);
-      if (len < 0) {
-        printf("[FWDD]read error, %d\n", errno);
-        return -1;
-      }
-      offset += len;
-      left_len -= len;
-    }
-    // data
-    int w_fd = open((const char *)fmeta->filename, O_CREAT | O_RDWR, 0644);
-    if (w_fd < 0) {
-      printf("[FWDD]open error, %d\n", errno);
+    ret = recv_sync(fd, fmeta->filename, f_length);
+    if (ret) {
+      printf("recv file name error\n");
       return -1;
     }
-    left_len = data_len - sizeof(ForwardRequest) -
-               sizeof(ForwardNode) * req.ttl - sizeof(ForwardFile) - f_length;
-    while (left_len > 0) {
-      len = read(fd, buffer, 16384);
-      if (len < 0) {
-        printf("[FWDD]read error, %d\n", errno);
+    if (req.ttl > 0) {
+      forward_next(fd, &req, fnodes, fmeta);
+      free(fnodes);
+    } else {
+      // data
+      int w_fd = open((const char *)fmeta->filename, O_CREAT | O_RDWR, 0644);
+      if (w_fd < 0) {
+        printf("[FWDD]open error, %d\n", errno);
         return -1;
       }
-      left_len -= len;
-      int write_len = write(w_fd, buffer, len);
-      if (write_len != len) {
-        printf("[FWDD]write error, %d %d\n", write_len, errno);
+      ret = forward_sync(fd, w_fd, req.length - sizeof(ForwardRequest) -
+                                       sizeof(ForwardNode) * req.ttl -
+                                       sizeof(ForwardFile) - f_length);
+      if (ret) {
+        printf("[FWDD]forward data error\n");
         return -1;
       }
+      printf("[FWDD]data write to %s success\n", fmeta->filename);
+      close(w_fd);
     }
-    printf("[FWDD]data write to %s success\n", fmeta->filename);
     free(fmeta);
-    close(w_fd);
     close(fd);
   }
   return 0;
@@ -179,7 +240,7 @@ int main(int argc, char *argv[]) {
   }
   // parent
   if (pid > 0) {
-    printf("[FWDD]parent exit, child pid: %d\n", pid);
+    // printf("[FWDD]parent exit, child pid: %d\n", pid);
     return 0;
   }
   if (setsid() < 0) {
@@ -194,7 +255,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
   if (pid > 0) {
-    printf("[FWDD]parent exit, child pid: %d\n", pid);
+    // printf("[FWDD]parent exit, child pid: %d\n", pid);
     return 0;
   }
   umask(0);
